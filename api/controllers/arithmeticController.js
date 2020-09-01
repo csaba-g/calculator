23f6c0f1b0ae2da1d47fbae0cc81cab8cc514100
csaba-g <50528815+csaba-g@users.noreply.github.com>
2020-09-01 06:06:14 -0700

    added comment to explain addition
    code is efficient, but confusing

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    // Using + iperator to coerce variables to numbes to avoud string concatenation
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
